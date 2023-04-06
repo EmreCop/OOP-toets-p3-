4a0288b682634210001343b860a94beb4dd4fe2f
EmreCop <99261888+EmreCop@users.noreply.github.com>
2023-04-06 10:35:37 +0200

    Update schoolklas.cs

--- schoolklas.cs
+++ schoolklas.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//ik wordt hoet well gedebuffed door dat niet kan eten.
+//ik wordt hier well gedebuffed door dat niet kan eten.
 
 namespace School
 {
